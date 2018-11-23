c3aa2b071f2d510d666ebb47c86a7872ad4daa61
boat <59010383@kmitl.ac.th>
2018-11-23 18:01:57 +0700

    Button Main User

--- app/src/main/java/com/example/asus/project_alert/MainUser.java
+++ app/src/main/java/com/example/asus/project_alert/MainUser.java
@@ -11,27 +11,13 @@ import android.widget.ListView;
 
 public class MainUser extends AppCompatActivity {
 
-    String[] Movies = new String[]{
-            "Harry Portter Season 1",
-            "Harry Portter Season 2",
-            "Harry Portter Season 3",
-            "Harry Portter Season 4",
-            "Harry Portter Season 5",
-            "Harry Portter Season 6",
-            "Harry Portter Season 7",
-            "Fantastic Beast",
-    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_user);
 
         ImageButton alert = findViewById(R.id.alert);
-        ListView movieList = (ListView)findViewById(R.id.textView);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Movies);
-
-        movieList.setAdapter(adapter);
 
         alert.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,5 +26,13 @@ public class MainUser extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+    }
+
+    public void onClickNext (View view) {
+        Button bt_intent = findViewById(R.id.intent);
+        Intent intent = new Intent(MainUser.this,showAlert.class);
+        startActivity(intent);
     }
 }
